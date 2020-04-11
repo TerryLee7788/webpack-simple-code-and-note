6e9426eb4be8b41a086773a63e45992ad5657c97
TerryLee <k9114658@gmail.com>
2020-04-12 00:58:16 +0800

    update for test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.jsx';
+
+const add = (a, b) => a + b;
+// eslint-disable-next-line no-console
+console.log(add(1, 2));
+
 
 ReactDOM.render(
     <App />,
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,58 @@
 module.exports = {
-    // https://webpack.js.org/configuration/entry-context
-    entry: [
-        './src/index.js'
+// https://webpack.js.org/configuration/entry-context
+  entry: [
+    './src/index.js',
+  ],
+  // https://webpack.js.org/configuration/output
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  // https://webpack.js.org/configuration/dev-server
+  devServer: {
+    contentBase: './dist',
+    // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    // writetodisk: true
+  },
+  // https://webpack.js.org/configuration/module
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true,
+            },
+          },
+        ],
+      },
     ],
-    // https://webpack.js.org/configuration/output
-    output: {
-        path: `${__dirname}/dist`,
-        publicPath: '/',
-        filename: '[name].js'
-    },
-    // https://webpack.js.org/configuration/dev-server
-    devServer: {
-        contentBase: './dist',
-        // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
-        writetodisk: true
-    },
-    // https://webpack.js.org/configuration/module
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader',
-                    // 'eslint-loader'
-                ]
-            },
-        ]
-    },
-    // https://webpack.js.org/configuration/resolve/
-    resolve: {
-        extensions: [
-            '.js',
-            '.jsx'
-        ]
-    },
-    // https://webpack.js.org/configuration/optimization/
-    optimization: {
-        // https://imweb.io/topic/5b66dd601402769b60847149
-        splitChunks: {
-            cacheGroups: {
-                vendors: {
-                    chunks: 'all',
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendor',
-                },
-            },
+  },
+  // https://webpack.js.org/configuration/resolve/
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+  // https://webpack.js.org/configuration/optimization/
+  optimization: {
+    // https://imweb.io/topic/5b66dd601402769b60847149
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          chunks: 'all',
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
         },
+      },
     },
+  },
 };
 
 // https://eslint.org/docs/user-guide/configuring
